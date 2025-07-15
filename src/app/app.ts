34fc327bf267bf9caa1278894ad1e125f8f7e2b8
Rolando Ceballos <rceballos@emaresa.cl>
2025-07-15 19:07:16 -0400

    add components, bootstrap, and angular materials

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Toolbar } from "./toolbar/toolbar";
+import { Navbar } from "./navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toolbar, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
