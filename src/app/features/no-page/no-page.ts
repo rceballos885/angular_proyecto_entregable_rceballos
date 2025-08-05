50ac1c7ca317dd35fc8199bdb5f0a0420002b98b
Rolando Ceballos <rceballos@emaresa.cl>
2025-08-05 18:38:54 -0400

    add new feature page error

---
+++ src/app/features/no-page/no-page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+// import {init} from './404.js';
+
+@Component({
+  selector: 'app-no-page',
+  imports: [],
+  templateUrl: './no-page.html',
+  styleUrl: './no-page.css'
+})
+export class NoPage {
+
+  
+
+}
+
