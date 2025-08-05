34e10fb0800f3c2bce715061900ceb367cbbc780
Rolando Ceballos <rceballos@emaresa.cl>
2025-08-05 18:45:19 -0400

    create file enum and create new routePath

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { RoutePaths } from '../shared/routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: RoutePaths.STUDENTS,
+        loadComponent: () => import('./features/students-table/students-table').then(m => m.StudentsTable)
+    },
+    {
+        path: RoutePaths.STUDENT,
+        loadComponent: () => import('./features/student/student').then(m => m.Student)
+    },
+    {
+        path: RoutePaths.REGISTER_ST,
+        loadComponent: () => import('./features/add-form/add-form').then(m => m.AddForm)
+    },
+    {
+        path: RoutePaths.REGISTER,
+        loadComponent: () => import('./features/registrations/registrations').then(m => m.Registrations)
+    },
+    {
+        path: RoutePaths.COURSES,
+        loadComponent: () => import('./features/courses/courses').then(m => m.Courses)
+    },
+    {
+        path: RoutePaths.NO_PAGE,
+        loadComponent: () => import('./features/no-page/no-page').then(m => m.NoPage)
+    }
+];
---
+++ src/shared/routes.ts
@@ -0,0 +1,9 @@
+export enum RoutePaths {
+    HOME = '',
+    STUDENTS = 'students',
+    STUDENT = 'student/:id',
+    REGISTER_ST = 'register-student',
+    REGISTER = 'register',
+    COURSES = 'courses',
+    NO_PAGE = '**'
+}
