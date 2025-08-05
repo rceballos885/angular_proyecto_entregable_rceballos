af2f22ffea1e23ba9c0285cc7ba57e137a9dad43
Rolando Ceballos <rceballos@emaresa.cl>
2025-08-05 18:44:15 -0400

    alter files into project on change for route

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,21 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
-@Output() activeSection = new EventEmitter<string>(); // Default active section
-
+// @Output() activeSection = new EventEmitter<string>(); // Default active section
   constructor() {
     // Initialization logic can go here
   }
 
   // Method to change the active section
-  changeSection(section: string) {
-    this.activeSection.emit(section);
-    console.log(`Active section changed to: ${section}`);
-  }
+  // changeSection(section: string) {
+  //   this.activeSection.emit(section);
+  //   console.log(`Active section changed to: ${section}`);
+  // }
 }
