dfab69179c7504db3c8a8e889dfbf55081edb7f1
Rolando Ceballos <rceballos@emaresa.cl>
2025-08-25 09:32:28 -0400

    add libraries ngrx and config zone.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { authReducer } from './ngrx/session/auth.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideStore({
+      // Define your reducers here
+      // Example: myFeature: myFeatureReducer
+      usersLogin: authReducer,
+    })
+]
 };
